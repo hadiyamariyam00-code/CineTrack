1945143693c00b7a76bad5f6a82de47673ec479f
Hadiya Mariyam <hadiyamariyam00@gmail.com>
2026-09-10 12:29:23 +0530

    Add base app shell and global styles

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
+import WatchListPage from './pages/WatchListPage'
+import WatchedPage from './pages/WatchedPage'
+import StatsPage from './pages/StatsPage'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/watchlist" element={<WatchListPage />} />
+        <Route path="/watched" element={<WatchedPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+      </Routes>
+     </div>
+  )
+}
+
+export default App
