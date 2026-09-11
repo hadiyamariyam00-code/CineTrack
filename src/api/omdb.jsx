487e2cd6fafdbb970cad74ba3c86b6e15a91680f
Hadiya Mariyam <hadiyamariyam00@gmail.com>
2026-09-11 13:40:13 +0530

    Load API key from env and add getMovieById function

--- src/api/omdb.jsx
+++ src/api/omdb.jsx
@@ -1,8 +1,17 @@
-const API_KEY = '50c2a215'
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+console.log("API KEY:", API_KEY)
 
 export async function searchMovies(query) {
     const response = await fetch(
         `https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`
+    )
+    return response.json()
+}
+
+export async function getMovieById(imdbID) {
+    const response = await fetch(
+        `https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`
     )
     return response.json()
 }
